dcb5fd3c8b255224d22462a05debaa63d0e7627b
Osar <oscar_mcintosh@msn.com>
2023-10-30 12:14:15 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    // rules: [
+    //   {
+    //     test: /\.css$/,
+    //     use: ['style-loader', 'css-loader'],
+    //   },
+    // ],
+  },
+  devtool: 'eval-source-map',
+  watch: true,
+}
