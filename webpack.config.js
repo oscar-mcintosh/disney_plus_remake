b4a7076a4b45b9b8e7e372024b74c0a94829683f
Osar <oscar_mcintosh@msn.com>
2023-11-15 16:52:32 -0600

    Updated routing

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const pageNames = ['index', 'disney.html', 'marvel.html', 'pixar.html', 'starwars.html', 'natgeo.html', 'movie-details'];
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const plugins = [
-  new MiniCssExtractPlugin(),
-  // Generate HTML files
-  ...pageNames.map(
-    (page) =>
-      new HtmlWebpackPlugin({
-        filename: `${page}.html`,
-        template: `./src/${page}.html`,
-      })
-  ),
-];
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
+    // path: path.resolve(__dirname, 'dist/assets'),
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
     assetModuleFilename: '[name][ext]',
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(__dirname, 'dist')
     },
     port: 3000,
     open: true,
     hot: true,
     compress: true,
-    historyApiFallback: true,
+    historyApiFallback: true
   },
   module: {
     rules: [
@@ -41,10 +31,42 @@ module.exports = {
       },
       {
         test: /\.(png|mp4)$/i,
-        type: 'asset/resource',
+        type: 'asset/resource'
       },
     ],
   },
-  plugins: [...plugins], // Include the plugins array here
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'disney.html',
+      template: './src/disney.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'marvel.html',
+      template: './src/marvel.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'pixar.html',
+      template: './src/pixar.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'starwars.html',
+      template: './src/starwars.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'natgeo.html',
+      template: './src/natgeo.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'movie-details.html',
+      template: './src/movie-details.html'
+    })
+
+  ],
   devtool: 'eval-source-map',
-};
+  // watch: true,
+}
